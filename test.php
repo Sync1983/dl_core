aafee306332e7d54c177945432d269fbc7df61da
Sync1983 <1983sync@gmail.com>
2015-10-29 01:44:02 +0300

    compile

---
+++ test.php
@@ -0,0 +1,14 @@
+<?php
+$b = [
+    15 => [
+      'text'  => 'abcdf',
+      'id'    =>  15
+    ],
+    18 => [
+      'text'  => 'b',
+      'id'    => 18
+    ]
+  ];
+$c = 'adf';
+$d = 0;
+echo dl_array($b,$c,$d);
